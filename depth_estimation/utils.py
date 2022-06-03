5c4d8f2d5e79ce3805fd8d335a96f4830bd675c3
ChienTM <chientm@vitalify.asia>
2022-06-03 17:34:20 +0700

    Model evaluation

---
+++ depth_estimation/utils.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+
+def visualize_depth_map(samples, test=False, model=None):
+    input, target = samples
+    cmap = plt.cm.jet
+    cmap.set_bad(color="black")
+
+    if test:
+        pred = model.predict(input)
+        fig, ax = plt.subplots(6, 3, figsize=(50, 50))
+        for i in range(6):
+            ax[i, 0].imshow((input[i].squeeze()))
+            ax[i, 1].imshow((target[i].squeeze()), cmap=cmap)
+            ax[i, 2].imshow((pred[i].squeeze()), cmap=cmap)
+
+    else:
+        fig, ax = plt.subplots(6, 2, figsize=(50, 50))
+        for i in range(6):
+            ax[i, 0].imshow((input[i].squeeze()))
+            ax[i, 1].imshow((target[i].squeeze()), cmap=cmap)
